c8912fae2cde1abe57a852941f7a09d34a37aefd
urtekin17 <urtekinnuri@gmail.com>
2025-11-23 00:47:56 +0300

    Stock service arrangements

---
+++ MultiSiteStockChecker.API/Application/Interfaces/IStockService.cs
@@ -0,0 +1,16 @@
+﻿using Core.Models;
+using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IStockService
+    {
+        Task<List<StockResult>> SearchForStock(string stockCode);
+        Task<StockResult> CheckStockAsync(Site site, IPage page, string stockCode, Dictionary<string, string> parserDetails);
+    }
+}
